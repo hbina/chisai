ad900f23e420a07c6d02278d309b78bb0aedf1a8
hanif.ariffin.4326@gmail.com <Hanif Bin Ariffin>
2020-08-07 20:04:36 -0400

    First works in trying to generalize and improve the codebase
    Currently, we simply "hard code" the text to various languages.
    This is not quite sustainable and correct.
    For instance, the integral representations in various language might be different.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 extern crate clap;
 
-use clap::{crate_version, App, Arg};
-use rayon::prelude::*;
 use std::fs;
 use std::io::prelude::*;
+
+use clap::{crate_version, App, Arg};
+use rayon::prelude::*;
+
+#[derive(Debug, Copy, Clone)]
+enum Format {
+    Binary,
+    Octal,
+    LowerHex,
+    UpperHex,
+}
+
+impl From<&str> for Format {
+    fn from(x: &str) -> Self {
+        match x {
+            "binary" => Format::Binary,
+            "octal" => Format::Octal,
+            "lowerhex" => Format::LowerHex,
+            "upperhex" => Format::UpperHex,
+            _ => Format::Octal,
+        }
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let matches = App::new("chisai")
@@ -12,22 +33,22 @@ fn main() -> std::io::Result<()> {
         .about("Transform binaries into embeddable code.")
         // Required arguments.
         .arg(
-            Arg::with_name("language")
-                .long("language")
-                .help("Desired language")
-                .takes_value(true)
-                .index(1)
-                .required(true)
-                .help("Desired language of the generated code."),
-        )
-        .arg(
             Arg::with_name("input-file-name")
                 .long("input-file-name")
                 .help("Input file")
                 .takes_value(true)
-                .index(2)
+                .index(1)
                 .required(true)
                 .help("Input file"),
+        )
+        .arg(
+            Arg::with_name("language")
+                .long("language")
+                .help("Desired language")
+                .takes_value(true)
+                .index(2)
+                .required(true)
+                .help("Desired language of the generated code."),
         )
         .arg(
             Arg::with_name("output-file-name")
@@ -53,6 +74,12 @@ fn main() -> std::io::Result<()> {
                 .long("no-const")
                 .help("Generated variables are mutable."),
         )
+        .arg(
+            Arg::with_name("format")
+                .long("format")
+                .help("The format of the values.")
+                .takes_value(true),
+        )
         // NOTE: I don't quite get what this means...
         .arg(
             Arg::with_name("always-escape")
@@ -60,10 +87,10 @@ fn main() -> std::io::Result<()> {
                 .help("Always escape every byte with an octal escape."),
         )
         .arg(
-            Arg::with_name("line-length")
-                .long("line-length")
+            Arg::with_name("variable-per-line")
+                .long("variable-per-line")
                 .takes_value(true)
-                .help("WIP: Append every Nth character with a newline."),
+                .help("For every N variable, append a newline."),
         )
         .arg(
             Arg::with_name("ignore-whitespace")
@@ -74,72 +101,59 @@ fn main() -> std::io::Result<()> {
 
     let language = matches.value_of("language").unwrap();
     let input_file_name = matches.value_of("input-file-name").unwrap();
-    let output_file_name = matches.value_of("output-file-name").unwrap();
+    let output_file_name = matches.value_of("output-file-name");
     let output_variable_name = matches.value_of("output-variable-name").unwrap_or("stdin");
     let always_escape = matches.is_present("always-escape");
     let disable_const = matches.is_present("no-const");
-    let output_file = matches.value_of("output-file-name");
+    let ignore_whitespace = matches.is_present("ignore-whitespace");
+    let format = matches.value_of("format").unwrap_or("ii");
 
     dbg!(
-        "language:{} input_file:{} output_file:{} always_escape:{} output_variable_name:{} disable_const:{}",
-        language, input_file_name, output_file_name, always_escape, output_variable_name,disable_const
+        language,
+        input_file_name,
+        output_file_name,
+        always_escape,
+        output_variable_name,
+        disable_const,
+        ignore_whitespace,
+        format
     );
 
-    let r = '\r' as i32;
-    let n = '\n' as i32;
-    let t = '\t' as i32;
-    let q = '\"' as i32;
-    let s = '\\' as i32;
-    // TODO: Is it possible to integrate rayon?
-    let content = fs::read(input_file_name)
-        .expect("Something went wrong reading the file.")
-        .par_iter()
-        // TODO: It would be amazing if we could preallocate...
-        .fold(
-            || String::new(),
-            |mut acc: String, x: &u8| {
-                let c = *x as i32;
-
-                if always_escape {
-                    acc.push('\\' as u8 as char);
-                    acc.push(('0' as u8 + ((c & 0o700) >> 6) as u8) as char);
-                    acc.push(('0' as u8 + ((c & 0o070) >> 3) as u8) as char);
-                    acc.push(('0' as u8 + ((c & 0o007) >> 0) as u8) as char);
-                } else if c >= 32
-                    && c <= 126
-                    && c != '"' as i32
-                    && c != '\\' as i32
-                    && c != '?' as i32
-                    && c != ':' as i32
-                    && c != '%' as i32
-                {
-                    acc.push(c as u8 as char)
-                } else if c == r {
-                    acc.push('\r' as u8 as char);
-                } else if c == n {
-                    acc.push('\n' as u8 as char);
-                } else if c == t {
-                    acc.push('\t' as u8 as char);
-                } else if c == q {
-                    acc.push('\"' as u8 as char);
-                } else if c == s {
-                    acc.push('\\' as u8 as char);
-                } else {
-                    acc.push('\\' as u8 as char);
-                    acc.push(('0' as u8 + ((c & 0o700) >> 6) as u8) as char);
-                    acc.push(('0' as u8 + ((c & 0o070) >> 3) as u8) as char);
-                    acc.push(('0' as u8 + ((c & 0o007) >> 0) as u8) as char);
-                }
-                acc
-            },
-        )
-        .reduce(
-            || String::new(),
-            |mut a: String, b: String| {
-                a.push_str(&b);
-                a
-            },
-        );
+    let content = if let Ok(x) = fs::read(input_file_name) {
+        let mut content = x
+            .par_iter()
+            .fold(
+                || String::new(),
+                |mut s: String, c: &u8| {
+                    match format.into() {
+                        Format::Binary => {
+                            s += &format!("{:#b}, ", c);
+                        }
+                        Format::Octal => {
+                            s += &format!("0x{:#o}, ", c);
+                        }
+                        Format::LowerHex => {
+                            s += &format!("{:#x}, ", c);
+                        }
+                        Format::UpperHex => {
+                            s += &format!("{:#X}, ", c);
+                        }
+                    }
+                    s
+                },
+            )
+            .reduce(
+                || String::new(),
+                |mut a: String, b: String| {
+                    a.push_str(&b);
+                    a
+                },
+            );
+        content.truncate(content.len() - 2);
+        content
+    } else {
+        panic!("Something went wrong while reading the file.")
+    };
 
     let output_string = match language {
         "c" | "cpp" | "c++" => {
@@ -148,7 +162,7 @@ fn main() -> std::io::Result<()> {
             } else {
                 "const "
             };
-            format!("{}unsigned char {}[] = \"{}\";", constness, output_variable_name, content)
+            format!("{}unsigned char {}[] = {{\n{}\n}};", constness, output_variable_name, content)
         }
         "python" | "py" => {
             format!("{}: str = {};", output_variable_name, content)
@@ -159,7 +173,7 @@ fn main() -> std::io::Result<()> {
         _ => panic!(format!("Unknown language:{}. If you think this tool should support this language extension, please submit a PR.", language))
     };
 
-    match output_file {
+    match output_file_name {
         Some(filename) => {
             let mut file = std::fs::File::create(filename)?;
             file.write_all(output_string.as_bytes())?;
